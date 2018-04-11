4fcd8f4a2d5963d27aaa97f72bbff6a215798493
Chris L <chris.luginbuhl@gmail.com>
2018-04-11 14:38:35 -0400

    Added microphone input and neopixel output.
    The microphone reads max sound level over a 10ms sample window repeatedly, and flashes the neopixels 4s after each sound that exceeds 6dB above the baseline 59dB level. 
    
    Loud noises (15dB or more above baseline) stops the beeping for 15s and causes the neopixels to flash red.

--- Sputnik/Sputnik.ino
+++ Sputnik/Sputnik.ino
@@ -6,6 +6,11 @@
 */
 
 #include <Adafruit_CircuitPlayground.h>
+#define SAMPLE_WINDOW   5  // Sample window for average level  - 5ms means 200Hz sound is the lowest we'll measure
+#define SOUND_FLOOR     59  // Lower range of mic sensitivity in dB SPL
+#define SOUND_CEILING  110  // Upper range of mic sensitivity in db SPL
+#define SOUND_THRESHOLD 100
+#define NUM_PEAKS 20
 
 const int toneDuration = 250;  //duration in milliseconds
 const int toneFreq = 800;      //frequency in Hz
@@ -16,44 +21,102 @@ const int minObserved = 240;
 const int maxObserved = 500;
 
 
+int soundPeak[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+}; //20 zeroes
+int peakIndex = 0;
 long timer = 0;
 long beepTimer = 0;
 long sensorSum = 0;
 int beepDelay = 4000;
 int movement = 0;
 int sensorValue = 0;        // analog value read from the radar 0-1023
+int numPixels = 10;
+uint32_t color = 0;
+bool mute = false;
+int muteTimeout = 0;
 
 void setup() {
 
   Serial.begin(9600);
   CircuitPlayground.begin();
-
+  CircuitPlayground.setBrightness(255);
+  CircuitPlayground.clearPixels();
+  numPixels = CircuitPlayground.strip.numPixels();
 }
 
 uint8_t effect = 1;
 
 
 void loop() {
+  
+  int peakToPeak = 0;
+  float peakToPeakSum = 0;
+ 
   sensorSum = 0;
+
+  //read from radar and mic (with average for smoothing)
   for (int i = 0; i < numSamples; i++) {
     sensorSum += analogRead(analogInPin);
-    delay(3); //let sensor settle after read
+    peakToPeakSum += CircuitPlayground.mic.soundPressureLevel(SAMPLE_WINDOW);
   }
+
+
   sensorValue = sensorSum / numSamples - analogRead(analogPotPin);         //note this is integer division. Should be fine with these values.
   movement = map(sensorValue, minObserved, maxObserved, 0, 100);  //empirically determined upper and lower bounds mapped to a percent scale
   movement = constrain(movement, 0, 100);         //clip anything outside this range
-  //  movement = (2*movement + pow(movement, 2) / 100) / 3; //inverse bell curve to make beeping less frequent without reducing possibility to go to 100%
-  beepDelay = map(movement, 0, 100, 4000, 250);
+  //  movement = (2*movement + pow(movement, 2) / 100) / 3; //inverse bell curve to make beeping less frequent without reducing the
+  //possibility to go to 100%
+  beepDelay = map(movement, 0, 100, 4000, 250);  //sets max and min time between beeps
+
+  //"play back" (with LEDs) the last sound exceeding the threshold
+
+  if (soundPeak[peakIndex + 1] > (SOUND_FLOOR + 3) && (soundPeak[peakIndex + 1] <= (SOUND_FLOOR + 10))) {
+    color = CircuitPlayground.strip.Color(255, 255, 175); // whitish light - contrasting slightly with blue interior of sputnik.
+  } else if (soundPeak[peakIndex + 1] > (SOUND_FLOOR + 20)) {
+    color = CircuitPlayground.strip.Color(255, 40, 10);
+  } else {
+    color = CircuitPlayground.strip.Color(0, 0, 0);
+  }
+  for (int i = 0; i < numPixels; i++) {
+    CircuitPlayground.strip.setPixelColor(i, color);
+  }
+  CircuitPlayground.strip.show();
+
+
+  //Cycle to the next spot on the array, wrapping around if at the end
+  if (peakIndex < (NUM_PEAKS - 2)) {
+    peakIndex++;
+  } else {
+    peakIndex = 0;
+  }
+
+  peakToPeak = int(peakToPeakSum / numSamples);
+  soundPeak[peakIndex] = peakToPeak; //storing the last 20 values in an array in order to "play them back" with LEDs
+ 
+  if (peakToPeak > (SOUND_FLOOR + 10))
+  {
+    Serial.println("Mute!");
+    mute = true;
+    muteTimeout = millis();
+  } else if ((millis() - muteTimeout) > 15000) {
+    mute = false;
+    Serial.println("....unmute!");
+  }
 
   if ((millis() - beepTimer) > beepDelay) {
-    if (CircuitPlayground.slideSwitch()) {
-      CircuitPlayground.playTone(toneFreq, toneDuration, false); //false is a flag to set non-blocking behaviour
+    if (CircuitPlayground.slideSwitch() && !mute) {
+      CircuitPlayground.playTone(toneFreq, toneDuration, true); //true sets blocking behaviour - don't want beep to register in mic
     }
     beepTimer = millis();
   }
 
+
+
   // print the results to the Serial Monitor:
-  Serial.print("sensor = ");
+  Serial.print("Sound: ");
+  Serial.print(peakToPeak);
+  Serial.print("\t motion sensor = ");
   Serial.print(sensorValue);
   Serial.print("\t pot = ");
   Serial.print(analogRead(analogPotPin));
@@ -61,4 +124,11 @@ void loop() {
   Serial.print(movement);
   Serial.print("\t beepDelay = ");
   Serial.println(beepDelay);
+  for (int i = 0; i < 20; i++) {
+    Serial.print(soundPeak[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+
 }
